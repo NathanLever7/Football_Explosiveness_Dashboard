c48d123e1115ff4d53830b725df247569489537d
NathanLever7 <129379760+NathanLever7@users.noreply.github.com>
2023-09-10 10:20:03 +0100

    Update Explosiveness_Dashboard.py

--- Explosiveness_Dashboard.py
+++ Explosiveness_Dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+from datetime import datetime
 
 # Base directory where the CSV files are stored. 
 # This assumes that the data folders are in the same directory as your script.
@@ -9,23 +10,36 @@ base_dir = os.path.dirname(__file__)
 import pandas as pd
 import requests
 
-def load_data(league, season):
+def load_data(league, seasons):
     base_url = "https://raw.githubusercontent.com/NathanLever7/Football_Explosiveness_Dashboard/main/"
-    season_dir = f'{league.replace(" ", "_")}_{season.replace("/", "_")}/'
     
-    # Load data from GitHub using raw file URLs
-    team_explosiveness_url = f"{base_url}{season_dir}Team_Explosiveness.csv"
-    opposition_explosiveness_url = f"{base_url}{season_dir}Opposition_Explosiveness.csv"
-    player_explosiveness_url = f"{base_url}{season_dir}Player_Explosiveness.csv"
-    player_efficiency_url = f"{base_url}{season_dir}Player_Efficiency.csv"
+    data = {}  # Create a dictionary to store data for each season
     
-    # Fetch data using requests
-    team_explosiveness = pd.read_csv(team_explosiveness_url, encoding='utf-8-sig')
-    opposition_explosiveness = pd.read_csv(opposition_explosiveness_url, encoding='utf-8-sig')
-    player_explosiveness = pd.read_csv(player_explosiveness_url, encoding='utf-8-sig')
-    player_efficiency = pd.read_csv(player_efficiency_url, encoding='utf-8-sig')
+    for season in seasons:
+        season_dir = f'{league.replace(" ", "_")}_{season.replace("/", "_")}/'
+        
+        # Load data from GitHub using raw file URLs
+        team_explosiveness_url = f"{base_url}{season_dir}Team_Explosiveness.csv"
+        opposition_explosiveness_url = f"{base_url}{season_dir}Opposition_Explosiveness.csv"
+        player_explosiveness_url = f"{base_url}{season_dir}Player_Explosiveness.csv"
+        player_efficiency_url = f"{base_url}{season_dir}Player_Efficiency.csv"
+        
+        # Fetch data using requests
+        team_explosiveness = pd.read_csv(team_explosiveness_url, encoding='utf-8-sig')
+        opposition_explosiveness = pd.read_csv(opposition_explosiveness_url, encoding='utf-8-sig')
+        player_explosiveness = pd.read_csv(player_explosiveness_url, encoding='utf-8-sig')
+        player_efficiency = pd.read_csv(player_efficiency_url, encoding='utf-8-sig')
+        
+        # Store the data in the dictionary
+        data[season] = {
+            'Team Explosiveness': team_explosiveness,
+            'Opposition Explosiveness': opposition_explosiveness,
+            'Player Explosiveness': player_explosiveness,
+            'Player Efficiency': player_efficiency,
+        }
     
-    return team_explosiveness, opposition_explosiveness, player_explosiveness, player_efficiency
+    return data
+
 
 
 # Streamlit UI
@@ -38,8 +52,12 @@ season = st.sidebar.selectbox('Select Season', ['2023/24', '2022/23'])  # Add ot
 # Load the data for the selected league and season
 team_explosiveness, opposition_explosiveness, player_explosiveness, player_efficiency = load_data(league, season)
 
-# Display data and visualizations
+# Display horizontal bar chart for team explosiveness
 st.header('Team Explosiveness')
-st.write(team_explosiveness)
+team_explosiveness_data = data[season]['Team Explosiveness']
+st.bar_chart(team_explosiveness_data.set_index('Team Name')[['Explosiveness']])
 
-# ... (add more Streamlit elements to display other data and create visualizations)
+# Display last updated date and time
+st.text(f'Last updated: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+
+
