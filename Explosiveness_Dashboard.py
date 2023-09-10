7c8debacc1ecc0ad1c398127865688f822ac95a7
NathanLever7 <129379760+NathanLever7@users.noreply.github.com>
2023-09-10 10:15:19 +0100

    Update Explosiveness_Dashboard.py

--- Explosiveness_Dashboard.py
+++ Explosiveness_Dashboard.py
@@ -6,15 +6,27 @@ import os
 # This assumes that the data folders are in the same directory as your script.
 base_dir = os.path.dirname(__file__)
 
-# Function to load data for a selected league and season
+import pandas as pd
+import requests
+
 def load_data(league, season):
-    # Construct the season directory using a relative path
-    season_dir = os.path.join(base_dir, 'Explosiveness Seasons', f'{league.replace(" ", "_")}_{season.replace("/", "_")}')
-    team_explosiveness = pd.read_csv(os.path.join(season_dir, 'Team_Explosiveness.csv'), encoding='utf-8-sig')
-    opposition_explosiveness = pd.read_csv(os.path.join(season_dir, 'Opposition_Explosiveness.csv'), encoding='utf-8-sig')
-    player_explosiveness = pd.read_csv(os.path.join(season_dir, 'Player_Explosiveness.csv'), encoding='utf-8-sig')
-    player_efficiency = pd.read_csv(os.path.join(season_dir, 'Player_Efficiency.csv'), encoding='utf-8-sig')
+    base_url = "https://raw.githubusercontent.com/NathanLever7/Football_Explosiveness_Dashboard/main/"
+    season_dir = f'{league.replace(" ", "_")}_{season.replace("/", "_")}/'
+    
+    # Load data from GitHub using raw file URLs
+    team_explosiveness_url = f"{base_url}{season_dir}Team_Explosiveness.csv"
+    opposition_explosiveness_url = f"{base_url}{season_dir}Opposition_Explosiveness.csv"
+    player_explosiveness_url = f"{base_url}{season_dir}Player_Explosiveness.csv"
+    player_efficiency_url = f"{base_url}{season_dir}Player_Efficiency.csv"
+    
+    # Fetch data using requests
+    team_explosiveness = pd.read_csv(team_explosiveness_url, encoding='utf-8-sig')
+    opposition_explosiveness = pd.read_csv(opposition_explosiveness_url, encoding='utf-8-sig')
+    player_explosiveness = pd.read_csv(player_explosiveness_url, encoding='utf-8-sig')
+    player_efficiency = pd.read_csv(player_efficiency_url, encoding='utf-8-sig')
+    
     return team_explosiveness, opposition_explosiveness, player_explosiveness, player_efficiency
+
 
 # Streamlit UI
 st.title('Football Analysis Dashboard')
