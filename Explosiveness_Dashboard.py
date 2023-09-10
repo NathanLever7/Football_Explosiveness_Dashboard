b5b4bbc7829518819d62baeb331aa278f0e7891b
NathanLever7 <129379760+NathanLever7@users.noreply.github.com>
2023-09-10 10:42:07 +0100

    Update Explosiveness_Dashboard.py

--- Explosiveness_Dashboard.py
+++ Explosiveness_Dashboard.py
@@ -1,50 +1,44 @@
 import streamlit as st
 import pandas as pd
 import os
-from datetime import datetime  # Import the datetime module
+from datetime import datetime
 
 # Base directory where the CSV files are stored.
-# This assumes that the data folders are in the same directory as your script.
 base_dir = os.path.abspath(os.path.dirname(__file__))
-
-import pandas as pd
-import requests
 
 def load_data(league, season):
     base_url = "https://raw.githubusercontent.com/NathanLever7/Football_Explosiveness_Dashboard/main/"
     season_dir = f'{league.replace(" ", "_")}_{season.replace("/", "_")}/'
-
-    # Debugging: Check the values of season_dir and team_explosiveness_url
-    st.write(f'season_dir: {season_dir}')
-    st.write(f'team_explosiveness_url: {team_explosiveness_url}')
-
     
     # Load data from GitHub using raw file URLs
     team_explosiveness_url = f"{base_url}{season_dir}Team_Explosiveness.csv"
-    opposition_explosiveness_url = f"{base_url}{season_dir}Opposition_Explosiveness.csv"
-    player_explosiveness_url = f"{base_url}{season_dir}Player_Explosiveness.csv"
-    player_efficiency_url = f"{base_url}{season_dir}Player_Efficiency.csv"
+    
+    # Debugging: Check the URL
+    st.write(f'Team explosiveness URL: {team_explosiveness_url}')
     
     # Fetch data using requests
     team_explosiveness = pd.read_csv(team_explosiveness_url, encoding='utf-8-sig')
-    opposition_explosiveness = pd.read_csv(opposition_explosiveness_url, encoding='utf-8-sig')
-    player_explosiveness = pd.read_csv(player_explosiveness_url, encoding='utf-8-sig')
-    player_efficiency = pd.read_csv(player_efficiency_url, encoding='utf-8-sig')
     
-    return team_explosiveness, opposition_explosiveness, player_explosiveness, player_efficiency
+    # Debugging: Display the DataFrame
+    st.write('Team explosiveness DataFrame:', team_explosiveness)
+    
+    return team_explosiveness
 
 # Streamlit UI
 st.title('Football Analysis Dashboard')
 
 # Let users select a league and season
-league = st.sidebar.selectbox('Select League', ['Premier League'])  # Add other leagues to the list as needed
-season = st.sidebar.selectbox('Select Season', ['2023/24', '2022/23'])  # Add other seasons to the list as needed
+league = st.sidebar.selectbox('Select League', ['Premier League'])
+season = st.sidebar.selectbox('Select Season', ['2023/24', '2022/23'])
 
 # Load the data for the selected league and season
-team_explosiveness, opposition_explosiveness, player_explosiveness, player_efficiency = load_data(league, season)
+team_explosiveness = load_data(league, season)
 
+# Debugging: Check the columns of the DataFrame
+st.write('Columns:', team_explosiveness.columns)
 
 # Display last updated date and time
 st.text(f'Last updated: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+
 
 
