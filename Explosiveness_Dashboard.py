f3d6594ee3788a9b339863cc9229dbab1d2ac570
NathanLever7 <129379760+NathanLever7@users.noreply.github.com>
2023-09-10 15:18:58 +0100

    Update Explosiveness_Dashboard.py

--- Explosiveness_Dashboard.py
+++ Explosiveness_Dashboard.py
@@ -1,61 +1,47 @@
-import streamlit as st
-import pandas as pd
-import os
-from datetime import datetime
-import requests
-import matplotlib.pyplot as plt
-from matplotlib import colors
-
-# Base directory where the CSV files are stored.
-# This assumes that the data folders are in the same directory as your script.
-base_dir = os.path.abspath(os.path.dirname(__file__))
-
-def load_data(league, season):
+def load_data(league, season, analysis_type):
     base_url = "https://raw.githubusercontent.com/NathanLever7/Football_Explosiveness_Dashboard/main/"
     season_dir = f'{league.replace(" ", "_")}_{season.replace("/", "_")}/'
     
     # Load data from GitHub using raw file URLs
-    team_explosiveness_url = f"{base_url}{season_dir}Team_Explosiveness.csv"
+    explosiveness_url = f"{base_url}{season_dir}{analysis_type}_Explosiveness.csv"
     
     # Explicitly set column names when reading the CSV
-    team_explosiveness = pd.read_csv(
-        team_explosiveness_url,
+    explosiveness_data = pd.read_csv(
+        explosiveness_url,
         encoding='utf-8-sig',
         names=['Squad', 'Team Explosiveness Index']  # Replace with actual column names
     )
+    explosiveness_data['Team Explosiveness Index'] = pd.to_numeric(explosiveness_data['Team Explosiveness Index'], errors='coerce')
+    return explosiveness_data
+
+def plot_data(team_explosiveness, league, season, analysis_type):
+    # Convert 'Team Explosiveness Index' to numeric
     team_explosiveness['Team Explosiveness Index'] = pd.to_numeric(team_explosiveness['Team Explosiveness Index'], errors='coerce')
-    return team_explosiveness
 
-# Streamlit UI
-st.title('Football Analysis Dashboard')
+    # Create a colormap
+    norm = colors.Normalize(vmin=team_explosiveness['Team Explosiveness Index'].min(), vmax=team_explosiveness['Team Explosiveness Index'].max())
+    cmap = plt.cm.get_cmap("coolwarm")
+    color_values = cmap(norm(team_explosiveness['Team Explosiveness Index'].values))
 
-# Let users select a league and season
-league = st.sidebar.selectbox('Select League', ['Premier League'])  # Add other leagues to the list as needed
-season = st.sidebar.selectbox('Select Season', ['2023/24', '2022/23'])  # Add other seasons to the list as needed
+    plt.figure(figsize=(12, 8))
+    plt.barh(team_explosiveness['Squad'], team_explosiveness['Team Explosiveness Index'], color=color_values)
+    plt.xlabel('Team Explosiveness Index')
+    plt.ylabel('Team')
+    plt.title(f'{analysis_type} Explosiveness Index {league} {season}')
+    plt.gca().invert_yaxis()
 
-# Load the data for the selected league and season
-team_explosiveness = load_data(league, season)
+    # Display the plot in the Streamlit application
+    st.pyplot(plt.gcf())
 
-# Remove the first row from the DataFrame
-team_explosiveness = team_explosiveness.iloc[1:]
+# In your main part of the script, you would call these functions like this:
 
-# Convert 'Team Explosiveness Index' to numeric
-team_explosiveness['Team Explosiveness Index'] = pd.to_numeric(team_explosiveness['Team Explosiveness Index'], errors='coerce')
+# Load and plot team data
+team_data = load_data(league, season, "Team")
+plot_data(team_data.iloc[1:], league, season, "Team")
 
-# Create a colormap
-norm = colors.Normalize(vmin=team_explosiveness['Team Explosiveness Index'].min(), vmax=team_explosiveness['Team Explosiveness Index'].max())
-cmap = plt.cm.get_cmap("coolwarm")
-color_values = cmap(norm(team_explosiveness['Team Explosiveness Index'].values))
-
-plt.figure(figsize=(12, 8))
-plt.barh(team_explosiveness['Squad'], team_explosiveness['Team Explosiveness Index'], color=color_values)
-plt.xlabel('Team Explosiveness Index')
-plt.ylabel('Team')
-plt.title(f'Team Explosiveness Index {league} {season}')
-plt.gca().invert_yaxis()
-
-# Display the plot in the Streamlit application
-st.pyplot(plt.gcf())
+# Load and plot opposition data
+opposition_data = load_data(league, season, "Opposition")
+plot_data(opposition_data.iloc[1:], league, season, "Opposition")
 
 st.text(f'Last updated: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
