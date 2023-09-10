4d17d44111ece335beeb88e11d92f0daa9b699bf
NathanLever7 <129379760+NathanLever7@users.noreply.github.com>
2023-09-10 15:30:05 +0100

    Update Explosiveness_Dashboard.py

--- Explosiveness_Dashboard.py
+++ Explosiveness_Dashboard.py
@@ -6,67 +6,65 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 
 # Base directory where the CSV files are stored.
-# This assumes that the data folders are in the same directory as your script.
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
-def load_data(league, season, analysis_type):
+def load_data(league, season, data_type):
     base_url = "https://raw.githubusercontent.com/NathanLever7/Football_Explosiveness_Dashboard/main/"
     season_dir = f'{league.replace(" ", "_")}_{season.replace("/", "_")}/'
     
-    # Load data from GitHub using raw file URLs
-    explosiveness_url = f"{base_url}{season_dir}{analysis_type}_Explosiveness.csv"
+    data_url = f"{base_url}{season_dir}{data_type}.csv"
     
-    # Explicitly set column names when reading the CSV
-    explosiveness_data = pd.read_csv(
-        explosiveness_url,
-        encoding='utf-8-sig',
-        names=['Squad', f'{analysis_type} Explosiveness Index']  # Replace with actual column names
-    )
-    explosiveness_data[f'{analysis_type} Explosiveness Index'] = pd.to_numeric(explosiveness_data[f'{analysis_type} Explosiveness Index'], errors='coerce')
-    return explosiveness_data
-
-def plot_data(explosiveness_data, league, season, analysis_type):
-    # Remove the first row from the DataFrame
-    explosiveness_data = explosiveness_data.iloc[1:]
-
-    # Convert 'Explosiveness Index' to numeric
-    explosiveness_data[f'{analysis_type} Explosiveness Index'] = pd.to_numeric(explosiveness_data[f'{analysis_type} Explosiveness Index'], errors='coerce')
-
-    # Create a colormap
-    norm = colors.Normalize(vmin=explosiveness_data[f'{analysis_type} Explosiveness Index'].min(), vmax=explosiveness_data[f'{analysis_type} Explosiveness Index'].max())
+    data_df = pd.read_csv(data_url, encoding='utf-8-sig')
     
-    # Choose a different colormap for the opposition data
-    if analysis_type == "Opposition":
-        cmap = plt.cm.get_cmap("coolwarm_r")  # '_r' stands for reversed colormap
-    else:
-        cmap = plt.cm.get_cmap("coolwarm")
-    
-    color_values = cmap(norm(explosiveness_data[f'{analysis_type} Explosiveness Index'].values))
-
-    plt.figure(figsize=(12, 8))
-    plt.barh(explosiveness_data['Squad'], explosiveness_data[f'{analysis_type} Explosiveness Index'], color=color_values)
-    plt.xlabel(f'{analysis_type} Explosiveness Index')
-    plt.ylabel('Team')
-    plt.title(f'{analysis_type} Explosiveness Index {league} {season}')
-    plt.gca().invert_yaxis()
-
-    # Display the plot in the Streamlit application
-    st.pyplot(plt.gcf())
-
+    return data_df
 
 # Streamlit UI
 st.title('Football Analysis Dashboard')
 
 # Let users select a league and season
-league = st.sidebar.selectbox('Select League', ['Premier League'])  # Add other leagues to the list as needed
-season = st.sidebar.selectbox('Select Season', ['2023/24', '2022/23'])  # Add other seasons to the list as needed
+league = st.sidebar.selectbox('Select League', ['Premier League'])  
+season = st.sidebar.selectbox('Select Season', ['2023/24', '2022/23'])  
 
-# Now, call your functions with the defined league and season variables
-team_data = load_data(league, season, "Team")
-plot_data(team_data, league, season, "Team")
+# Load the data for the selected league and season
+team_explosiveness = load_data(league, season, "Team_Explosiveness")
+opposition_explosiveness = load_data(league, season, "Opposition_Explosiveness")
 
-opposition_data = load_data(league, season, "Opposition")
-plot_data(opposition_data, league, season, "Opposition")
+# Plot Team Explosiveness data
+st.subheader('Team Explosiveness Data')
+norm = colors.Normalize(vmin=team_explosiveness['Team Explosiveness Index'].min(), vmax=team_explosiveness['Team Explosiveness Index'].max())
+cmap = plt.cm.get_cmap("coolwarm")
+color_values = cmap(norm(team_explosiveness['Team Explosiveness Index'].values))
+
+plt.figure(figsize=(12, 8))
+plt.barh(team_explosiveness['Squad'], team_explosiveness['Team Explosiveness Index'], color=color_values)
+plt.xlabel('Team Explosiveness Index')
+plt.ylabel('Team')
+plt.title(f'Team Explosiveness Index {league} {season}')
+plt.gca().invert_yaxis()
+st.pyplot(plt.gcf())
+
+# Plot Opposition Explosiveness data
+st.subheader('Opposition Explosiveness Data')
+norm = colors.Normalize(vmin=opposition_explosiveness['Team Explosiveness Index'].min(), vmax=opposition_explosiveness['Team Explosiveness Index'].max())
+cmap = plt.cm.get_cmap("coolwarm_r")  # Reversed colormap
+color_values = cmap(norm(opposition_explosiveness['Team Explosiveness Index'].values))
+
+plt.figure(figsize=(12, 8))
+plt.barh(opposition_explosiveness['Squad'], opposition_explosiveness['Team Explosiveness Index'], color=color_values)
+plt.xlabel('Team Explosiveness Index')
+plt.ylabel('Team')
+plt.title(f'Opposition Explosiveness Index {league} {season}')
+plt.gca().invert_yaxis()
+st.pyplot(plt.gcf())
+
+# Load and display Player Explosiveness data
+player_explosiveness_data = load_data(league, season, "Player_Explosiveness")
+st.subheader('Player Explosiveness Data')
+st.dataframe(player_explosiveness_data)
+
+# Load and display Player Efficiency data
+player_efficiency_data = load_data(league, season, "Player_Efficiency")
+st.subheader('Player Efficiency Data')
+st.dataframe(player_efficiency_data)
 
 st.text(f'Last updated: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-
