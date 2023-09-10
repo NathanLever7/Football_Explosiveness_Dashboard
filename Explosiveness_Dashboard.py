c3acd34008092496b6c61b3eec39d5fdb594faef
NathanLever7 <129379760+NathanLever7@users.noreply.github.com>
2023-09-10 10:05:28 +0100

    Update Explosiveness_Dashboard.py

--- Explosiveness_Dashboard.py
+++ Explosiveness_Dashboard.py
@@ -2,12 +2,14 @@ import streamlit as st
 import pandas as pd
 import os
 
-# Base directory where the CSV files are stored
-base_dir = "C:\\Users\\natha\\Documents\\Football\\Explosiveness"
+# Base directory where the CSV files are stored. 
+# This assumes that the data folders are in the same directory as your script.
+base_dir = os.path.dirname(__file__)
 
 # Function to load data for a selected league and season
 def load_data(league, season):
-    season_dir = os.path.join(base_dir, f'{league.replace(" ", "_")}_{season.replace("/", "_")}')
+    # Construct the season directory using a relative path
+    season_dir = os.path.join(base_dir, 'Explosiveness Seasons', f'{league.replace(" ", "_")}_{season.replace("/", "_")}')
     team_explosiveness = pd.read_csv(os.path.join(season_dir, 'Team_Explosiveness.csv'), encoding='utf-8-sig')
     opposition_explosiveness = pd.read_csv(os.path.join(season_dir, 'Opposition_Explosiveness.csv'), encoding='utf-8-sig')
     player_explosiveness = pd.read_csv(os.path.join(season_dir, 'Player_Explosiveness.csv'), encoding='utf-8-sig')
@@ -19,7 +21,7 @@ st.title('Football Analysis Dashboard')
 
 # Let users select a league and season
 league = st.sidebar.selectbox('Select League', ['Premier League'])  # Add other leagues to the list as needed
-season = st.sidebar.selectbox('Select Season', ['2023/24'])  # Add other seasons to the list as needed
+season = st.sidebar.selectbox('Select Season', ['2023/24', '2022/23'])  # Add other seasons to the list as needed
 
 # Load the data for the selected league and season
 team_explosiveness, opposition_explosiveness, player_explosiveness, player_efficiency = load_data(league, season)
